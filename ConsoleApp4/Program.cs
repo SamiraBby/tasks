e253486626e067670f433f8c685b7ae942508288
SamiraBby <samiraob@code.edu.az>
2022-08-05 01:32:17 -0700

    first task

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,333 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region sinifde yazilmis kodlarin evde tekrari ( baxmasanizda olar buna :))
+            //short a = 5;
+            //int b = a;
+            //Console.WriteLine(b); //implicit or automaticly
+
+            //int a = 7;
+            //float c = a;
+            //Console.WriteLine(c);
+
+            //float a = 6.1f;
+            //int c = (int)a;
+            //Console.WriteLine(c); //explicit or manual
+
+            //DateTime dt = DateTime.Now;
+            //DateTime dt2 = dt;
+            //dt = new DateTime(2018, 7, 24);
+
+            //Console.WriteLine(dt2);
+            //Console.WriteLine(dt.ToString());
+            //    string name = "Samire";
+            //    int age = 24;
+            //    if (name == "Samire")
+            //    {
+            //        Console.WriteLine("s");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sehv yazirsan");
+            //    }
+            //String name = "tural";
+            //int age = 26;
+            //if(name=="Tural")
+            //{
+            //    Console.WriteLine("o");
+            //}
+            //else if (name == "Samire")
+            //{
+            //    Console.WriteLine("h");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("1");
+            //}
+
+
+
+            //String name = "tural";
+            //int age = 26;
+
+            //if (name == "Tural" && age ==5)
+            //{
+            //    Console.WriteLine("o");
+            //}
+            //else if (name == "Samire")
+            //{
+            //    Console.WriteLine("h");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("1");
+            //}
+
+
+
+            //string name ="Tyral";
+            // string result = (name =="tural") ?"Duzdur adin":"Sehvdir adin";
+            //Console.WriteLine(result);
+
+            //string name = "Tural";
+            //string result = name == "tural" ? "o" : name == "Samira" ? "h" : "1";
+            //Console.WriteLine(result);    
+            //int ?a = null;
+            //int age = 2;
+            //while (age<6)
+            //{
+            //    Console.WriteLine(age);
+            //    age++;
+            //}
+            //while (a<=10)
+            //{
+            //    Console.WriteLine(a);
+            //    a++;
+            //}
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    if (i==3)
+            //    {
+            //        Console.WriteLine("3dur");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //int age = 10;
+            //do
+            //{
+            //    Console.WriteLine(age);
+            //    age++;
+            //} while (age<20 );
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //int i = 5;
+            //while (i <10)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //int i = 5;
+            //do
+            //{
+            //    i++;
+
+            //    Console.WriteLine(i);
+            //} while (i < 10);
+            //Console.WriteLine("Lütfen bir sayı giriniz");
+            //int sayi = Int32.Parse(Console.ReadLine());
+
+
+            //switch (sayi)
+            //{
+
+            //    case 1: Console.WriteLine("Pazartesi"); break;
+            //    case 2: Console.WriteLine("Salı"); break;
+            //    case 3: Console.WriteLine("Çarşamba"); break;
+            //    case 4: Console.WriteLine("Perşembe"); break;
+            //    case 5: Console.WriteLine("Cuma"); break;
+            //    case 6: Console.WriteLine("Cumartesi"); break;
+            //    case 7: Console.WriteLine("Pazar"); break;
+
+            //    default: Console.WriteLine("Hata Böyle bir gün yok"); break;
+            //}
+
+            //int age = 2;
+            //switch (age)
+            //{
+            //    case 1: Console.WriteLine("azdi"); break;
+            //    case 2: Console.WriteLine("2cidi"); break;
+            //    default:
+            //        Console.WriteLine(age);
+            //        break;
+            //}
+            //int age = 5;
+            //switch (age)
+            //{
+            //    case 0: Console.WriteLine("0"); ; break;
+            //        case 1: Console.WriteLine("1"); ; break;
+            //        default: Console.WriteLine("ustu"); ; break;
+            //}
+            //int weekday = 5;
+            //switch (weekday)
+            //{
+            //    case 1: Console.WriteLine("bazar ertesi"); break;
+            //        case 2: Console.WriteLine("cersembe axsami");break;
+            //        case 3: Console.WriteLine("cersembe");break;
+            //        case 4: Console.WriteLine("cume axsami");break;
+            //        case 5: Console.WriteLine("cume");break;
+            //        case 6: Console.WriteLine("sembe");break;
+            //        case 7: Console.WriteLine("bazar");break;
+            //        default: Console.WriteLine("bele gun yoxdur"); break;
+
+            //}
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 5) break;
+            //    Console.WriteLine( i);
+            //}
+
+            //string name;
+            //Console.WriteLine("Adinizi daxil edin");
+            //name= Console.ReadLine();
+            //Console.WriteLine("siz kece bilersiniz");
+
+            //string age;
+            //Console.WriteLine("yasinizi daxil edin");
+            //age = Console.ReadLine();
+            //if (int.Parse(age) > 18) 
+            //{
+            //    Console.WriteLine("siz keche bilersiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("keche bilmezsiniz");
+            //}
+
+            //string name;
+            //Console.WriteLine("adini");
+            //name = Console.ReadLine();
+            //if (name=="Samire")
+            //{
+            //    Console.WriteLine("keche bilersiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("adin duzgunlyuune emin olun");
+            //}
+            //string username = "Samire";
+
+            //string password = "123";
+            //Console.WriteLine("istifadeci adiniz");
+            //string username_check = Console.ReadLine();
+
+
+            //Console.WriteLine("istifadeci sifreniz");
+
+            //string password_check = Console.ReadLine();
+
+            //if (username == username_check && password == password_check)
+            //{  
+            //    Console.WriteLine("Xosh geldiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sehv melumat");
+            //}
+
+
+            //string[] students = new string[5] { "Samire", "Tural", "Aytac", "Emil", "Tayfun" };
+            //Console.WriteLine(students[0]);
+            //Console.WriteLine(students[1]); 
+            //string[] students = new string[5] { "Samire", "Tural", "Aytac", "Emil", "Tayfun" };
+            //string[] students = { "Samire", "Tural", "Aytac", "Emil", "Tayfun" };
+            //for (int i = 0; i < students.Length; i++)
+            //{
+            //    Console.WriteLine(students[i]);
+            //}
+
+            #endregion
+
+            // Homework
+
+            #region Task1(0-40 cut ededleri goster)
+            //for (int i = 1; i < 41; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+            #endregion
+
+            #region Task1-in daha dinamik formasi( her hansi reqem daxil etdikde cut tek olmasini gosterir)
+            //Console.WriteLine("Reqem daxil edin");
+            //string strNumb = Console.ReadLine();    
+            //int intNumb =int.Parse(strNumb);
+            //if (intNumb == 0)
+            //{
+            //    Console.WriteLine(intNumb + " ne tek ededdir , ne de cut");
+            //}
+            //else if (intNumb % 2 == 0) 
+            //{
+            //    Console.WriteLine(intNumb + " cut ededdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(intNumb +" tek ededdir");
+            //}
+            #endregion
+
+            #region Task3(cevrenin perimetri(uzunlugu) ve sahesini tapmaq
+            //double pi = 3.14;
+            //Console.WriteLine("radiusun uzunlugunu daxil edin");
+            //string strRad = Console.ReadLine();
+            //int intRad = int.Parse(strRad);
+            //double uzunluq = 2 * pi * intRad;
+            //double sahe = pi * intRad * intRad;
+
+
+            //Console.WriteLine("uzunluq : " + uzunluq);
+            //Console.WriteLine("sahe : " + sahe);
+
+            #endregion
+
+            #region task2
+            //int sum = 0;
+            //int[] myNum = {10,11,12,13,14,15,16,17,18,19,20,20,22,23,24,25,26,27,28,29,30 };
+            //for (int i = 0; i < myNum.Length; i++)
+            //{
+            //    if (myNum[i] % 2 == 1) 
+            //    {
+            //        sum+=myNum[i];
+
+
+            //    }
+
+
+
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+    }
+
